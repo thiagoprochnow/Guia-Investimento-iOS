7927ef1e4fd2e7a0a114bcc3d02fd8f73c4497f9
Thiago <thiprochnow@gmail.com>
2018-05-27 16:38:13 -0300

    Set correct background and status bar colors
    Set correct background and status bar colors.

---
+++ Guia Investimento/Code/PortfolioMainController.swift
@@ -0,0 +1,19 @@
+//
+//  PortfolioMainView.swift
+//  Guia Investimento
+//
+//  Created by Felipe on 27/05/18.
+//  Copyright © 2018 Thiago. All rights reserved.
+//
+
+import UIKit
+
+class PortfolioMainController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Titulo
+        self.title = "Carros"
+        self.view.backgroundColor = UIColor(red: 237/255, green: 237/255, blue: 237/255, alpha: 1)
+    }
+}
