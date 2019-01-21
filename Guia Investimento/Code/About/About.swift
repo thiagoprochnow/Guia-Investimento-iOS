9e2f264b8b1ef4626f541d218711df22201079c0
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2019-01-21 21:02:12 -0200

    Added terms of use and privacy policy link in About
    Added terms of use and privacy policy link in About

--- Guia Investimento/Code/About/About.swift
+++ Guia Investimento/Code/About/About.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class About: UIViewController {
     @IBOutlet var version: UILabel!
+    @IBOutlet var terms: UITextView!
+    @IBOutlet var privacy: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,5 +20,20 @@ class About: UIViewController {
         
         let versionString = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
         version.text = "Versão: " + versionString
+        
+        let attributedTerms = NSMutableAttributedString(string: "Terms of Use")
+        let attributedPrivacy = NSMutableAttributedString(string: "Privacy Policy")
+        let urlTerms = URL(string: "http://www.guiainvestimento.com.br/terms_use.html")
+        let urlPrivacy = URL(string: "http://www.guiainvestimento.com.br/privacy_policy.html")
+        
+        attributedTerms.setAttributes([.link: urlTerms], range: NSMakeRange(0, 12))
+        attributedPrivacy.setAttributes([.link: urlPrivacy], range: NSMakeRange(0, 14))
+        
+        self.terms.attributedText = attributedTerms
+        self.terms.isUserInteractionEnabled = true
+        self.terms.isEditable = false
+        self.privacy.attributedText = attributedPrivacy
+        self.privacy.isUserInteractionEnabled = true
+        self.privacy.isEditable = false
     }
 }
